aa765db13609af406a15fb17e0c4d4481fffed37
amin-da <darabniya@yahoo.com>
2021-11-21 11:01:28 +0330

    complete scroll-up featuer & add acive link for nav-menu items

--- js/index.js
+++ js/index.js
@@ -88,3 +88,33 @@ function endVideo() {
   videoIcon.classList.add("ri-play-line");
 }
 videoFile.addEventListener("ended", endVideo);
+
+
+// show scroll up button
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll__up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// SCROLL SECTIONS ACTIVE LINK 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
